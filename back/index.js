033b85ababd0cbabfc2890e52ea6d9a53ad23c9c
seogudwns <38065726+seogudwns@users.noreply.github.com>
2022-04-19 22:40:53 +0900

    기본설치모듈 완료.. README 읽어보셔요!!

---
+++ back/index.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.SERVER_PORT || 5000;
+
+app.get('/', (req,res) => {
+    console.log('접속메시지. 이게 보인다면 로컬에서 접속이 성공했다는 것이 콘솔창에 뜰 것입니다.')
+    res.send('10팀 데이터 프로젝트 시작!');
+});
+
+const Listening = () => {
+    console.log(`연결된 위치: http://localhost:${PORT}`);
+};
+
+app.listen(PORT, Listening);
