efefca0bd29509e1872f73b77791bae40fbebf91
seogudwns <38065726+seogudwns@users.noreply.github.com>
2022-04-20 18:59:51 +0900

    eslint 및 prettier 설치 완료

--- back/index.js
+++ back/index.js
@@ -1,20 +1,37 @@
-import "dotenv/config";
-import cors from "cors";
-import express from "express";
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+URL = process.env.MongoBD_URL || '주소에 접속이 되지 않았습니다.';
+
+mongoose.connect(URL);
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+	console.log('정상적으로 서버에 연결되었습니다.' + URL);
+	print(
+		'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+	);
+});
+db.on('error', error => console.error('MongoDB 연결에 실패하였습니다...\n' + URL + '\n' + error));
+
+// ===========================================
 
 app.use(cors());
 
 const PORT = process.env.SERVER_PORT || 5000;
 
-app.get('/', (req,res) => {
-    console.log('접속메시지. 이게 보인다면 로컬에서 접속이 성공했다는 것이 콘솔창에 뜰 것입니다.')
-    res.send('10팀 데이터 프로젝트 시작!');
+app.get('/', (req, res) => {
+	console.log('접속메시지. 이게 보인다면 로컬에서 접속이 성공했다는 것이 콘솔창에 뜰 것입니다.');
+	res.send('10팀 데이터 프로젝트 시작!');
 });
 
 const Listening = () => {
-    console.log(`연결된 위치: http://localhost:${PORT}`);
+	console.log(`연결된 위치: http://localhost:${PORT}`);
 };
 
 app.listen(PORT, Listening);
---
+++ code_convention_setting.md
@@ -0,0 +1,28 @@
+# CCS(정리이기 때문에 구분짓기 위해 snake_case 사용.)
+
+## Common
+
+-   almost_camel_case
+-   tabwidth: 4
+-   end_comma\*\n: true
+-   end_semicolon: true
+-   maxline: 100
+-   use\_'': true
+-   blank*inside*{}: true
+-   no_single_var_in_arr_func: true
+-   set_md: true
+-   개행문자 유지..(가끔 뜨는 애러 제거.): true
+    <!-- - call-back_func: arrow_func   -->
+    <!-- - first_class_word: large  (X)  -->
+
+### 아래 3개는 프론트용
+
+차례대로
+
+-   JSX의 마지막 `>`를 다음 줄로 내릴지 여부
+-   JSX에 singe 쿼테이션 사용 여부
+-   혹시몰라서 넣어놓은 vue파일의 들여쓰기 여부.
+
+#### ref : https://ui.toast.com/fe-guide/ko_CODING-CONVENTION
+
+#### ref2 : https://techwell.wooritech.com/docs/tools/prettier/prettier-eslint-airbnb/
