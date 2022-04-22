062310b4c2e4c430bc725fac5b17b3519267f33e
vjvl95 <vjvl95@naver.com>
2022-04-22 09:51:56 +0900

    feat: 자리배치 및 팀원소개 추가

--- front/src/component/BalancEat.js
+++ front/src/component/BalancEat.js
@@ -5,24 +5,216 @@ const BalancEat = () => {
 	return (
 		<>
 			<Container>
-				<div style={{ margin: '200px 0 0 150px' }}>
-					<h1>균형잡인 식단의 필요성</h1>
-					<h4>우리가 균형잡힌 식단을 먹어야 하는 이유는</h4>
+				<div
+					style={{
+						margin: '200px 0 0 400px',
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+					}}
+				>
+					<div>
+						<h1>균형잡인 식단의 필요성</h1>
+						<h4>우리가 균형잡힌 식단을 먹어야 하는 이유는</h4>
+					</div>
+					<img
+						style={{ margin: '0 100px 0 0' }}
+						src="/img/unknown.png"
+						width="500px"
+					></img>
 				</div>
 
-				<div style={{ margin: '200px 0 0 150px' }}>
-					<h1>균형잡인 식단의 필요성</h1>
-					<h4>우리가 균형잡힌 식단을 먹어야 하는 이유는</h4>
+				<div
+					style={{
+						margin: '200px 0 0 400px',
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+					}}
+				>
+					<div>
+						<h1>균형잡인 식단의 필요성</h1>
+						<h4>우리가 균형잡힌 식단을 먹어야 하는 이유는</h4>
+					</div>
+					<img
+						style={{ margin: '0 100px 0 0' }}
+						src="/img/unknown.png"
+						width="500px"
+					></img>
 				</div>
 
-				<div style={{ margin: '200px 0 0 150px' }}>
-					<h1>균형잡인 식단의 필요성</h1>
-					<h4>우리가 균형잡힌 식단을 먹어야 하는 이유는</h4>
+				<div
+					style={{
+						margin: '200px 0 0 400px',
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+					}}
+				>
+					<div>
+						<h1>균형잡인 식단의 필요성</h1>
+						<h4>우리가 균형잡힌 식단을 먹어야 하는 이유는</h4>
+					</div>
+					<img
+						style={{ margin: '0 100px 0 0' }}
+						src="/img/unknown.png"
+						width="500px"
+					></img>
 				</div>
 
-				<div style={{ margin: '200px 0 0 150px' }}>
-					<h1>균형잡인 식단의 필요성</h1>
-					<h4>우리가 균형잡힌 식단을 먹어야 하는 이유는</h4>
+				<div
+					style={{
+						margin: '200px 0 0 400px',
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+					}}
+				>
+					<div>
+						<h1>균형잡인 식단의 필요성</h1>
+						<h4>우리가 균형잡힌 식단을 먹어야 하는 이유는</h4>
+					</div>
+					<img
+						style={{ margin: '0 100px 0 0' }}
+						src="/img/unknown.png"
+						width="500px"
+					></img>
+				</div>
+
+				<div>
+					<div
+						style={{
+							border: '2px solid black',
+							display: 'flex',
+							flexDirection: 'column',
+							margin: '300px',
+						}}
+					>
+						<h1>팀원 소개</h1>
+						<div
+							style={{
+								borderBottom: '1px solid #789',
+								flexDirection: 'row',
+							}}
+						>
+							<ul style={{ display: 'flex', margin: '10vh 0', lineStlye: 'none' }}>
+								<li
+									style={{
+										listStyle: 'none',
+										margin: '0 auto',
+										textAlign: 'center',
+									}}
+								>
+									<img
+										style={{
+											border: '1px solid gray',
+											borderRadius: '50%',
+											height: '10vw',
+											width: '10vw',
+										}}
+										src="/img/bmcho-pic.1a18473a818b64ff3b5f.png"
+									/>
+									<h2>이름</h2>
+									<h3>직무</h3>
+								</li>
+								<li
+									style={{
+										listStyle: 'none',
+										margin: '0 auto',
+										textAlign: 'center',
+									}}
+								>
+									<img
+										style={{
+											border: '1px solid gray',
+											borderRadius: '50%',
+											height: '10vw',
+											width: '10vw',
+										}}
+										src="/img/bmcho-pic.1a18473a818b64ff3b5f.png"
+									/>
+									<h2>이름</h2>
+									<h3>직무</h3>
+								</li>
+								<li
+									style={{
+										listStyle: 'none',
+										margin: '0 auto',
+										textAlign: 'center',
+									}}
+								>
+									<img
+										style={{
+											border: '1px solid gray',
+											borderRadius: '50%',
+											height: '10vw',
+											width: '10vw',
+										}}
+										src="/img/bmcho-pic.1a18473a818b64ff3b5f.png"
+									/>
+									<h2>이름</h2>
+									<h3>직무</h3>
+								</li>
+								<li
+									style={{
+										listStyle: 'none',
+										margin: '0 auto',
+										textAlign: 'center',
+									}}
+								>
+									<img
+										style={{
+											border: '1px solid gray',
+											borderRadius: '50%',
+											height: '10vw',
+											width: '10vw',
+										}}
+										src="/img/bmcho-pic.1a18473a818b64ff3b5f.png"
+									/>
+									<h2>이름</h2>
+									<h3>직무</h3>
+								</li>
+								<li
+									style={{
+										listStyle: 'none',
+										margin: '0 auto',
+										textAlign: 'center',
+									}}
+								>
+									<img
+										style={{
+											border: '1px solid gray',
+											borderRadius: '50%',
+											height: '10vw',
+											width: '10vw',
+										}}
+										src="/img/bmcho-pic.1a18473a818b64ff3b5f.png"
+									/>
+									<h2>이름</h2>
+									<h3>직무</h3>
+								</li>
+								<li
+									style={{
+										listStyle: 'none',
+										margin: '0 auto',
+										textAlign: 'center',
+									}}
+								>
+									<img
+										style={{
+											border: '1px solid gray',
+											borderRadius: '50%',
+											height: '10vw',
+											width: '10vw',
+										}}
+										src="/img/bmcho-pic.1a18473a818b64ff3b5f.png"
+									/>
+									<h2>이름</h2>
+									<h3>직무</h3>
+								</li>
+							</ul>
+						</div>
+					</div>
 				</div>
 			</Container>
 		</>
