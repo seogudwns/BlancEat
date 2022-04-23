a211aabeb596f588a9b026a8f21d1135200b2ed8
Isacc <dewrain331@gmail.com>
2022-04-23 12:03:53 +0900

    feat : Main.js 디자인 중

--- front/src/Main.js
+++ front/src/Main.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const StyledSection = styled.div`
 	display: flex;
-	width: 100%;
 	height: 646px;
+	background-image: url(${props => (props.bgImage ? props.bgImage : 'none')});
+	background-repeat: no-repeat;
+	background-position: center center;
+	background-size: 100% 100%;
 `;
 
 const StyledContents = styled.div`
 	flex: ${props => (props.flex ? props.flex : 'none')};
-	background-image: ${props => (props.bgImage ? props.bgImage : 'none')};
+	width: 100%;
+	height: 100%;
+	vertical-align: ${props => (props.va ? props.va : 'none')};
+	text-align: ${props => (props.ta ? props.ta : 'none')};
 `;
 
 const Main = () => {
 	const mainImages = ['/mainImage1.jpg', '/mainImage2.jpg', '/mainImage3.jpg'];
+	const [curImgIndex, setCurImgIndex] = useState(0);
 
 	return (
 		<>
-			<StyledSection>
-				<StyledContents>Hi</StyledContents>
+			<StyledSection bgImage={mainImages[curImgIndex]}>
+				<StyledContents>{}</StyledContents>
 			</StyledSection>
 			<StyledSection>
 				<StyledContents>Hi</StyledContents>
