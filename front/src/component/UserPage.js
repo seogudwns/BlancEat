35755f25f894f040f7888ad7cde260e61706a3e0
vjvl95 <vjvl95@naver.com>
2022-04-27 17:41:34 +0900

    fact:사용자페이지 기본정보 박스 완성

--- front/src/component/UserPage.js
+++ front/src/component/UserPage.js
@@ -20,7 +20,22 @@ const UserPage = () => {
 	`;
 	const StyledUserInfo = styled.div`
 		margin-left: 30px;
+		border: solid 1px black;
+		width: 50%;
+		max-width: 700px;
 	`;
+	const StyledUserInfoBox = styled.div`
+		padding-top: 13px;
+		color: gray;
+		font-size: 20px;
+		height: 50px;
+		border: 1px solid black;
+		margin-bottom: 10px;
+		border-radius: 10px;
+		width: 90%;
+	`;
+
+	const StyledUserBMIbox = styled.div``;
 	return (
 		<>
 			<StyledContainer>
@@ -34,8 +49,19 @@ const UserPage = () => {
 
 			<StyledContainer>
 				<StyledUserInfo>
-					<h1>기본 신체 정보</h1>
+					<h1 style={{ fontWeight: 'lighter' }}>기본 신체 정보</h1>
+					<div style={{ border: 'solid 1px black', marginBottom: '10px' }}></div>
+					<div style={{ display: 'flex' }}>
+						<div style={{ width: '30%' }}>gd</div>
+						<div style={{ width: '70%' }}>
+							<StyledUserInfoBox>연령 만 29 세/ 남성</StyledUserInfoBox>
+							<StyledUserInfoBox>키 175cm</StyledUserInfoBox>
+							<StyledUserInfoBox>몸무게 68kg</StyledUserInfoBox>
+						</div>
+					</div>
 				</StyledUserInfo>
+
+				<StyledUserInfo></StyledUserInfo>
 			</StyledContainer>
 		</>
 	);
