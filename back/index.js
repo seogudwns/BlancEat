6a67c311b7e14f2adf4051f36127a9daf47e81d2
seogudwns <38065726+seogudwns@users.noreply.github.com>
2022-04-27 10:18:32 +0900

    mvp-back 틀 구현

--- back/index.js
+++ back/index.js
@@ -1,36 +1,5 @@
 import 'dotenv/config';
-<<<<<<< HEAD
-import cors from 'cors';
-import express from 'express';
-import mongoose from 'mongoose';
-
-// swagger module
-import { swaggerUi, specs } from './src/modules/swagger.js';
-
-const app = express();
-
-URL = process.env.MongoBD_URL || '주소에 접속이 되지 않았습니다.';
-
-mongoose.connect(URL);
-
-const dd = (x, y, z) => {
-	console.log(x), console.log(y), console.log(z);
-};
-
-const db = mongoose.connection;
-
-db.on('connected', () => {
-	console.log('정상적으로 서버에 연결되었습니다.' + URL);
-});
-db.on('error', error => console.error('MongoDB 연결에 실패하였습니다...\n' + URL + '\n' + error));
-
-// ===========================================
-
-app.use(cors());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
-=======
 import { app } from './src/index.js';
->>>>>>> user_mvp_back
 
 const PORT = process.env.SERVER_PORT || 5000;
 
