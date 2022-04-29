85a3437e1238fdf33196437a56e2266f662ae8c0
Isacc <dewrain331@gmail.com>
2022-04-29 11:01:53 +0900

    feat : ModalPortal 삭제

--- front/src/ModalComp.js
+++ front/src/ModalComp.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import ModalPortal from './ModalPortal';
 import { Modal } from 'react-bootstrap';
 
 const ModalComp = ({ show, setShow, title, main, children }) => {
 	return (
-		<ModalPortal>
-			<Modal show={show} onHide={() => setShow(false)}>
-				<Modal.Header closeButton>
-					<Modal.Title>{title}</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>{main}</Modal.Body>
-				<Modal.Footer>{children}</Modal.Footer>
-			</Modal>
-		</ModalPortal>
+		<Modal show={show} onHide={() => setShow(false)}>
+			<Modal.Header closeButton>
+				<Modal.Title>{title}</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>{main}</Modal.Body>
+			<Modal.Footer>{children}</Modal.Footer>
+		</Modal>
 	);
 };
 
