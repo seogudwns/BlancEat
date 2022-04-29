d1aac9cf04a556a67461a862a15f3371c3d29594
Isacc <dewrain331@gmail.com>
2022-04-29 13:42:50 +0900

    refactor : Approach 이미지 슬라이드 변경

--- front/src/Contents/ContentApproach.js
+++ front/src/Contents/ContentApproach.js
@@ -1,27 +1,22 @@
-import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
 
-import * as Content from './styleContents';
+import React from 'react';
 
 const ContentApproach = () => {
-	const mainImages = ['/approach01.png', '/approach02.png', '/approach03.png'];
-	const [curImgIndex, setCurImgIndex] = useState(0);
 	return (
-		<Content.StyledSection bgImage={mainImages[curImgIndex]}>
-			<Content.StyledContents jc="center" ai="flex-end">
-				<Content.StyledImageButton
-					onClick={() => setCurImgIndex(0)}
-					{...(curImgIndex === 0 ? { color: '#00c14d' } : { color: 'white' })}
-				></Content.StyledImageButton>
-				<Content.StyledImageButton
-					onClick={() => setCurImgIndex(1)}
-					{...(curImgIndex === 1 ? { color: '#00c14d' } : { color: 'white' })}
-				></Content.StyledImageButton>
-				<Content.StyledImageButton
-					onClick={() => setCurImgIndex(2)}
-					{...(curImgIndex === 2 ? { color: '#00c14d' } : { color: 'white' })}
-				></Content.StyledImageButton>
-			</Content.StyledContents>
-		</Content.StyledSection>
+		<>
+			<Carousel fade indicators={false} prevIcon={null} nextIcon={null}>
+				<Carousel.Item>
+					<img src="/approach01.png" />
+				</Carousel.Item>
+				<Carousel.Item>
+					<img src="/approach02.png" />
+				</Carousel.Item>
+				<Carousel.Item>
+					<img src="/approach03.png" />
+				</Carousel.Item>
+			</Carousel>
+		</>
 	);
 };
 
