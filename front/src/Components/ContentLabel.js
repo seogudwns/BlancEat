cfa77e954b58bdd7c49e1b641b20af8e0f085ad0
theohong <theohong0234@gmail.com>
2022-04-29 12:52:32 +0900

    chore : 빼먹은 파일 추가

--- front/src/Components/ContentLabel.js
+++ front/src/Components/ContentLabel.js
@@ -1,57 +1,5 @@
-import styled from 'styled-components';
-import { Container } from 'react-bootstrap';
 import * as s from './Styles/styleContentLabel';
 
-const AssetContainer = styled(Container)`
-	display: flex;
-	flex-direction: row;
-	align-items: flex-start;
-	height: 45px;
-	margin: 48px 0;
-	padding: 0px;
-`;
-
-const LabelContainer = styled.div`
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: flex-start;
-	min-width: 600px;
-	height: 45px;
-	margin: 0 0 0 92px;
-	padding: 0;
-`;
-const TitleWrapper = styled.div`
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: center;
-	min-width: 280px;
-	height: 58px;
-	padding: 0 20px;
-	color: white;
-	background-color: #023d1a;
-`;
-const LabelTitle = styled.h2`
-	font-family: 'Elice Digital Baeum';
-	font-size: 2rem;
-	font-weight: 400;
-	line-height: 120%;
-	letter-spacing: 0.5px;
-	align: center;
-	margin: 0;
-`;
-
-const LabelText = styled.h2`
-	font-family: 'Elice Digital Baeum';
-	font-size: 2rem;
-	font-weight: 400;
-	line-height: 120%;
-	letter-spacing: 0.5px;
-	color: #765413;
-	align: center;
-	margin: 0 0 0 18px;
-`;
 const ContentLabel = ({ title, subtitle }) => {
 	return (
 		<s.AssetContainer fluid>
