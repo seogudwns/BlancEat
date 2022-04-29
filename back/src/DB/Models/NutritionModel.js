315118cb31df57d4b43da60e753b3e8eaf6c4d88
seogudwns <38065726+seogudwns@users.noreply.github.com>
2022-04-30 06:08:56 +0900

    .

--- back/src/DB/Models/NutritionModel.js
+++ back/src/DB/Models/NutritionModel.js
@@ -7,10 +7,10 @@ class Nutrition {
 		return await nutritionModel.findOne({ foodName });
 	}
 
-    // 음식 여러개 한번에 가져오기
-    static async findManyByName({ foodName }) {
-        return await nutritionModel.find({'foodName': { $in: foodName }});
-    }
+	// 음식 여러개 한번에 가져오기
+	static async findManyByName({ foodName }) {
+		return await nutritionModel.find({ foodName: { $in: foodName } });
+	}
 
 	// 음식 검색어 추천
 	static async findNameByKeyword({ keyword }) {
