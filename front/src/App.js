f79d9f51e2d1a0ab80bee61b396a727114ad2290
Isacc <dewrain331@gmail.com>
2022-04-30 17:49:12 +0900

    feat : FindPw 페이지

--- front/src/App.js
+++ front/src/App.js
@@ -9,6 +9,7 @@ import Footer from './Pages/Footer';
 import BalancEat from './Pages/BalancEat';
 import Userpage from './Pages/Userpage';
 import RegisterForm from './component/user/RegisterForm';
+import FindPw from './component/user/FindPw';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Container = styled.div`
@@ -48,6 +49,7 @@ const App = () => {
 					<Route path="/recommand" element={<Recommand />} />
 					<Route path="/userpage" element={<Userpage />} />
 					<Route path="/register" element={<RegisterForm />} />
+					<Route path="/findpw" element={<FindPw />} />
 				</Routes>
 				<Footer />
 			</BrowserRouter>
