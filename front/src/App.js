14d26e84c7bff5d2e2a6b7ef50f73188f79ab4e8
Isacc <dewrain331@gmail.com>
2022-05-07 17:22:52 +0900

    chore : merge 오류 해결 및 패키지 수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { useSetRecoilState } from 'recoil';
 
 import Header from './Pages/Header';
 import Main from './Pages/Main';
@@ -11,32 +10,10 @@ import BalancEat from './Pages/BalancEat';
 import Userpage from './Pages/Userpage';
 import Testpage from './Pages/Recommands/Testpage';
 
-import { loginState, userIdState } from './Pages/User/UserAtom';
-import { useEffect } from 'react';
-
 import theme from './Commons/theme';
 import { GlobalStyle, Container } from './Commons/GlobalStyle';
 
 const App = () => {
-	const setIsLogin = useSetRecoilState(loginState);
-	const setUserId = useSetRecoilState(userIdState);
-	const fetchCurrentUser = async () => {
-		const userToken = sessionStorage.getItem('userToken');
-		const userIdFromServer = sessionStorage.getItem('userId');
-		if (userToken !== null) {
-			try {
-				setIsLogin(true);
-				setUserId(userIdFromServer);
-			} catch (err) {
-				console.error(err);
-			}
-		}
-	};
-
-	useEffect(() => {
-		fetchCurrentUser();
-	}, []);
-
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyle />
